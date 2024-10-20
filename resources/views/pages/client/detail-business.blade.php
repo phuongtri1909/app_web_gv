02f24b4f2cf594d7edf036c9c45c82e37bf2409b
phuongtri1909 <tri2003bt@gmail.com>
2024-10-20 08:20:10 +0700

    create model all business

---
+++ resources/views/pages/client/detail-business.blade.php
@@ -0,0 +1,20 @@
+@extends('pages.layouts.page')
+@push('styles')
+    <style>
+        
+    </style>
+@endpush
+
+@push('scripts')
+    <script>
+        
+    </script>
+@endpush
+
+@section('content')
+    <section id="business" class="business mt-8rem">
+        <div class="container">
+           
+        </div>
+    </section>
+@endsection
