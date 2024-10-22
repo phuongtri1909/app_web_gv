387316b12699a2a7afb53c11a48eed3e887d411e
vunguqua10 <vunguqua10@gmail.com>
2024-10-22 20:27:07 +0700

    form ket noi giao thuong,form tu van phap luat,form gia nhap hoi

--- resources/views/pages/client/gv/form-customer.blade.php
+++ resources/views/pages/client/gv/form-customer.blade.php
@@ -4,10 +4,10 @@
 @push('child-styles')
     <style>
         .form-container {
-            background-color: white;
-            padding: 20px;
+            /* background-color: white; */
+            /* padding: 20px; */
             border-radius: 8px;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            /* box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); */
             width: 100%;
             max-width: 800px;
             margin: 0 auto;
@@ -31,33 +31,6 @@
             font-weight: bold;
         }
 
-        input[type="text"],
-        input[type="tel"],
-        input[type="email"],
-        textarea,
-        select {
-            width: 100%;
-            padding: 10px;
-            border-radius: 4px;
-            border: 1px solid #ccc;
-            font-size: 16px;
-        }
-
-       .submit-btn {
-            width: 200px;
-            padding: 10px;
-            background-color: #0056b3;
-            color: white;
-            border: none;
-            border-radius: 4px;
-            font-size: 18px;
-            cursor: pointer;
-            transition: background-color 0.3s ease;
-            display: block;
-            margin: 20px auto;
-            text-align: center;
-        }
-
        .submit-btn:hover {
             background-color: #004494;
         }
@@ -78,52 +51,64 @@
             object-fit: cover;
         }
         @media (max-width: 768px) {
-        .form-container {
-            padding: 15px;
-        }
         .banner {
                 height: auto;
             }
-        input[type="text"],
-        input[type="tel"],
-        input[type="email"],
-        textarea,
-        select {
-            font-size: 14px;
+        }
+        #form-customer {
+            position: relative;
+            margin: 30px auto;
+            /* padding: 20px 0px 20px 0px; */
+            max-width: 800px;
+            background-color: #f8f9fa;
+            border-radius: 8px;
+            background: url('{{ asset('images/logo.png') }}') no-repeat;
+            background-size: 30%;
+            background-attachment: fixed;
+            background-position: center center;
+            z-index: 1;
         }
 
-        .submit-btn {
-            padding: 8px;
-            font-size: 14px;
+        form {
+            position: relative;
+            z-index: 1;
+            background-color: rgba(255, 255, 255, 0.9);
+            /* padding: 20px; */
+            border-radius: 8px;
         }
-    }
+        .btn-success {
+            background-color: #0056b3;
+            transition: background-color 0.3s ease;
+        }
 
-    @media (max-width: 480px) {
-        .form-container {
+        .btn-success:hover {
+            background-color: #004494;
+        }
+        .btn-submit {
+            width: 200px;
             padding: 10px;
+            background-color: #0056b3;
+            color: white;
+            border: none;
+            border-radius: 4px;
+            font-size: 18px;
+            cursor: pointer;
+            transition: background-color 0.3s ease;
+            display: block;
+            margin: 20px auto;
+            text-align: center;
         }
-
-        input[type="text"],
-        input[type="tel"],
-        input[type="email"],
-        textarea,
-        select {
-            font-size: 12px;
+        .btn-submit:hover {
+            background-color: #004494;
         }
-
-        .submit-btn {
-            padding: 6px;
-            font-size: 12px;
-        }
-    }
     </style>
 @endpush
 
 @section('content-page')
     <section id="form-customer">
-        <div class="banner">
+        {{-- <div class="banner">
             <img src="{{asset('images/Vayvonkinhdoanh.jpg')}}" alt="Banner Image">
-        </div>
+        </div> --}}
         <div class="container my-5">
             <div class="row">
                 <div class="form-container">
@@ -135,13 +120,13 @@
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label for="full_name">Họ tên:</label>
-                                    <input type="text" id="full_name" name="full_name" class="form-control" required placeholder="Nhập họ tên">
+                                    <input type="text" id="full_name" name="full_name" class="form-control form-control-sm" required placeholder="Nhập họ tên">
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label for="birth_year">Năm sinh:</label>
-                                    <input type="text" id="birth_year" name="birth_year" class="form-control" required placeholder="Nhập năm sinh">
+                                    <input type="text" id="birth_year" name="birth_year" class="form-control form-control-sm" required placeholder="Nhập năm sinh">
                                 </div>
                             </div>
                         </div>
@@ -149,7 +134,7 @@
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label for="gender">Giới tính:</label>
-                                    <select id="gender" name="gender" class="form-control" required>
+                                    <select id="gender" name="gender" class="form-control form-control-sm" required>
                                         <option value="" disabled selected>Chọn giới tính</option>
                                         <option value="male">Nam</option>
                                         <option value="female">Nữ</option>
@@ -160,51 +145,53 @@
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label for="phone_number">Số điện thoại liên hệ:</label>
-                                    <input type="tel" id="phone_number" name="phone_number" class="form-control" required pattern="[0-9]{10}" placeholder="Nhập số điện thoại">
+                                    <input type="tel" id="phone_number" name="phone_number" class="form-control form-control-sm" required pattern="[0-9]{10}" placeholder="Nhập số điện thoại">
                                 </div>
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="residence_address">Địa chỉ cư trú:</label>
-                            <input type="text" id="residence_address" name="residence_address" class="form-control" required placeholder="Nhập địa chỉ cư trú">
+                            <input type="text" id="residence_address" name="residence_address" class="form-control form-control-sm" required placeholder="Nhập địa chỉ cư trú">
                         </div>
 
                         <div class="form-group">
                             <label for="business_address">Địa chỉ kinh doanh:</label>
-                            <input type="text" id="business_address" name="business_address" class="form-control" placeholder="Nhập địa chỉ kinh doanh">
+                            <input type="text" id="business_address" name="business_address" class="form-control form-control-sm" placeholder="Nhập địa chỉ kinh doanh">
                         </div>
 
                         <div class="form-group">
                             <label for="company_name">Tên doanh nghiệp/hộ kinh doanh:</label>
-                            <input type="text" id="company_name" name="company_name" class="form-control" placeholder="Nhập tên doanh nghiệp">
+                            <input type="text" id="company_name" name="company_name" class="form-control form-control-sm" placeholder="Nhập tên doanh nghiệp">
                         </div>
 
                         <div class="form-group">
                             <label for="business_field">Ngành nghề kinh doanh:</label>
-                            <input type="text" id="business_field" name="business_field" class="form-control" placeholder="Nhập ngành nghề kinh doanh">
+                            <input type="text" id="business_field" name="business_field" class="form-control form-control-sm" placeholder="Nhập ngành nghề kinh doanh">
                         </div>
 
                         <div class="form-group">
                             <label for="tax_code">Mã số thuế:</label>
-                            <input type="text" id="tax_code" name="tax_code" class="form-control" placeholder="Nhập mã số thuế">
+                            <input type="text" id="tax_code" name="tax_code" class="form-control form-control-sm" placeholder="Nhập mã số thuế">
                         </div>
 
                         <div class="form-group">
                             <label for="email">Email:</label>
-                            <input type="email" id="email" name="email" class="form-control" placeholder="Nhập email">
+                            <input type="email" id="email" name="email" class="form-control form-control-sm" placeholder="Nhập email">
                         </div>
 
                         <div class="form-group">
                             <label for="fanpage">Fanpage:</label>
-                            <input type="text" id="fanpage" name="fanpage" class="form-control" placeholder="Nhập fanpage">
+                            <input type="text" id="fanpage" name="fanpage" class="form-control form-control-sm" placeholder="Nhập fanpage">
                         </div>
 
                         <div class="form-group">
                             <label for="support_needs">Nhu cầu hỗ trợ, kết nối:</label>
-                            <textarea id="support_needs" name="support_needs" class="form-control" placeholder="Nhập nhu cầu hỗ trợ, kết nối"></textarea>
+                            <textarea id="support_needs" name="support_needs" class="form-control form-control-sm" placeholder="Nhập nhu cầu hỗ trợ, kết nối"></textarea>
                         </div>
 
-                        <button type="submit" class="submit-btn">Gửi</button>
+                        <div class="text-end ">
+                            <button type="submit" class="btn btn-success">Gửi</button>
+                        </div>
                     </form>
                 </div>
             </div>
