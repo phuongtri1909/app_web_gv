fd8be3c78651f45295eee0bbdf47f2f6fcc258e0
phuongtri1909 <tri2003bt@gmail.com>
2024-10-22 20:25:07 +0700

    update migrate update customer

--- database/migrations/2024_10_22_150955_update_customer_interest_table.php
+++ database/migrations/2024_10_22_150955_update_customer_interest_table.php
@@ -11,38 +11,23 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('customer_interest', function (Blueprint $table) {
-            // Kiểm tra và xóa khóa ngoại nếu tồn tại
-            if (Schema::hasColumn('customer_interest', 'bank_services_id')) {
-                $sm = Schema::getConnection()->getDoctrineSchemaManager();
-                $doctrineTable = $sm->listTableDetails('customer_interest');
-                if ($doctrineTable->hasForeignKey('customer_interest_bank_services_id_foreign')) {
-                    $table->dropForeign(['bank_services_id']);
-                }
-                $table->dropColumn('bank_services_id');
-            }
+        // Schema::table('customer_interest', function (Blueprint $table) {
 
-            if (Schema::hasColumn('customer_interest', 'interest_id')) {
-                $sm = Schema::getConnection()->getDoctrineSchemaManager();
-                $doctrineTable = $sm->listTableDetails('customer_interest');
-                if ($doctrineTable->hasForeignKey('customer_interest_interest_id_foreign')) {
-                    $table->dropForeign(['interest_id']);
-                }
-                $table->dropColumn('interest_id');
-            }
+        //     $table->dropForeign(['bank_services_id']);
+        //     $table->dropForeign(['interest_id']);
 
-            $table->dropColumn('time');
-            $table->string('birth_year');
-            $table->string('gender');
-            $table->string('residence_address');
-            $table->string('business_address');
-            $table->string('company_name');
-            $table->string('business_field');
-            $table->string('tax_code');
-            $table->string('email');
-            $table->string('fanpage')->nullable();
-            $table->text('support_needs');
-        });
+        //     $table->dropColumn(['bank_services_id', 'interest_id','time']);
+        //     $table->string('birth_year');
+        //     $table->string('gender');
+        //     $table->string('residence_address');
+        //     $table->string('business_address');
+        //     $table->string('company_name');
+        //     $table->string('business_field');
+        //     $table->string('tax_code');
+        //     $table->string('email');
+        //     $table->string('fanpage')->nullable();
+        //     $table->text('support_needs');
+        // });
     }
 
     public function down()
