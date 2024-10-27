dda0ef430b2b6c75d7fb591a4a5091bce197405f
phuongtri1909 <tri2003bt@gmail.com>
2024-10-27 11:41:08 +0700

    business_field

--- database/migrations/2024_10_26_223332_change_column_businesses.php
+++ database/migrations/2024_10_26_223332_change_column_businesses.php
@@ -12,17 +12,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('businesses', function (Blueprint $table) {
-            $table->unsignedBigInteger('ward_id')->nullable()->change();
-            $table->string('address')->nullable()->change();
-            $table->string('avt_businesses')->nullable()->change();
+            // $table->unsignedBigInteger('ward_id')->nullable()->change();
+            // $table->string('address')->nullable()->change();
+            // $table->string('avt_businesses')->nullable()->change();
     
-            $table->enum('status', ['pending', 'approved', 'rejected', 'other'])->default('pending')->change();
+            // $table->enum('status', ['pending', 'approved', 'rejected', 'other'])->default('pending')->change();
     
-            $table->unsignedBigInteger('business_fields')->nullable()->change();
-            $table->foreign('business_fields')
-                ->references('id')
-                ->on('business_fields')
-                ->onDelete('cascade');
+            // $table->unsignedBigInteger('business_fields')->nullable()->change();
+            // $table->foreign('business_fields')
+            //     ->references('id')
+            //     ->on('business_fields')
+            //     ->onDelete('cascade');
                 });
     }
 
