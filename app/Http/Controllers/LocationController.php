6218ecda4656ec5a0bdb72a86c55ced348297b18
phuongtri1909 <tri2003bt@gmail.com>
2024-12-03 14:53:19 +0700

    locations P17 and QGV

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -246,7 +246,7 @@ class LocationController extends Controller
         } catch (\Exception $e) {
             DB::rollBack();
             $this->cleanupUploadedFiles1($data);
-            return redirect()->back()->withInput()->with('error', 'Đăng ký địa điểm thất bại' . $e->getMessage());
+            return redirect()->back()->with('error', 'Đăng ký địa điểm thất bại' . $e->getMessage())->withInput();
         }
     }
 }
