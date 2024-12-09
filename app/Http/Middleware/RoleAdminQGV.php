3f2de86179b9011413bf5d3e49d5b2a24dcd0ac0
phuongtri1909 <tri2003bt@gmail.com>
2024-12-09 23:09:23 +0700

    layout admin

--- app/Http/Middleware/RoleAdminQGV.php
+++ app/Http/Middleware/RoleAdminQGV.php
@@ -18,7 +18,7 @@ class RoleAdminQGV
     {
         $unit = auth()->user()->unit;
         
-        if($unit->unit_code !== 'qgv') {
+        if($unit->unit_code !== 'QGV') {
             return redirect()->back()->with('error', 'Bạn không có quyền truy cập trang này');
         }
 
