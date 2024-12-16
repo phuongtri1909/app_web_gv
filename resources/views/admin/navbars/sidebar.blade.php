353a9d5e764f3025e6749bb0a4c6a2026bc859a7
vunguqua10 <vunguqua10@gmail.com>
2024-12-16 17:12:30 +0700

    icon khao sat

--- resources/views/admin/navbars/sidebar.blade.php
+++ resources/views/admin/navbars/sidebar.blade.php
@@ -508,7 +508,7 @@
                             href="{{ route('competitions.index', ['type' => 'survey']) }}">
                             <div
                                 class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-                                <i class="fa-solid fa-trophy text-dark icon-sidebar"></i>
+                                <i class="fa-solid fa-square-poll-horizontal text-dark icon-sidebar"></i>
                             </div>
                             <span class="nav-link-text ms-1">Khảo sát trực tuyến</span>
                         </a>
