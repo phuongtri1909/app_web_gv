df5424fcee22ccef73d5ba3ebc519d055e555780
phuongtri1909 <tri2003bt@gmail.com>
2024-12-19 01:03:12 +0700

    update auth zalo

--- app/Services/ZaloApiService.php
+++ app/Services/ZaloApiService.php
@@ -12,6 +12,39 @@ use Illuminate\Http\Exceptions\HttpResponseException;
 
 class ZaloApiService
 {
+
+    public function updateProfile($get_info,$get_phone = null)
+    {
+        $customer = Customer::where('id', $get_info['id'])->first();
+
+        if (!$customer) {
+            $customer = new Customer();
+            $customer->id = $get_info['id'];
+            $customer->customerName = $get_info['name'];
+
+            if (isset($get_info['picture']['data']['url'])) {
+                $customer->imageUrl = $get_info['picture']['data']['url'];
+            }
+            if (!empty($get_phone['data']['number'])) {
+                $customer->phone = $get_phone['data']['number'];
+            }
+            $customer->save();
+        } else {
+            $customer->customerName = $get_info['name'];
+
+            if (!empty($get_phone['data']['number'])) {
+                $customer->phone = $get_phone['data']['number'];
+            }
+
+            if (isset($get_info['picture']['data']['url'])) {
+                $customer->imageUrl = $get_info['picture']['data']['url'];
+            }
+            $customer->save();
+        }
+
+        return $customer;
+    }
+
     public function getProfile($accessToken)
     {
         try {
@@ -20,26 +53,6 @@ class ZaloApiService
             ])->get('https://graph.zalo.me/v2.0/me', [
                 'fields' => 'id,name,picture',
             ]);
-
-            $customer = Customer::where('id', $response['id'])->first();
-
-            if (!$customer) {
-                $customer = new Customer();
-                $customer->id = $response['id'];
-                $customer->customerName = $response['name'];
-               
-                if (isset($response['picture']['data']['url'])) {
-                    $customer->imageUrl = $response['picture']['data']['url'];
-                }
-                $customer->save();
-            }else{
-                $customer->customerName = $response['name'];
-               
-                if (isset($response['picture']['data']['url'])) {
-                    $customer->imageUrl = $response['picture']['data']['url'];
-                }
-                $customer->save();
-            }
 
             return $response->json();
         } catch (\Exception $e) {
@@ -89,7 +102,7 @@ class ZaloApiService
             $state = $state;
 
             $authUrl = "https://oauth.zaloapp.com/v4/permission?app_id={$appId}&redirect_uri={$redirectUri}&code_challenge={$codeChallenge}&state={$state}";
-           
+
             return redirect($authUrl);
         } catch (\Exception $e) {
             Log::error("redirect to zalo login: " . $e->getMessage());
@@ -108,16 +121,16 @@ class ZaloApiService
                 'code_verifier' => $codeVerifier,
                 'grant_type' => 'authorization_code',
             ]);
-            
+
             if (isset($response['error']) && $response['error'] == -14019) {
-                
+
                 return $this->redirectToZaloLogin();
-            }elseif(isset($response['error']) && $response['error'] !== 0){
+            } elseif (isset($response['error']) && $response['error'] !== 0) {
                 return $this->redirectToZaloLogin();
             }
 
             // Save access token and refresh token
-         
+
             Session::put('access_token', $response['access_token']);
             Session::put('refresh_token', $response['refresh_token']);
             Session::put('refresh_token_expires_in', $response['refresh_token_expires_in']);
@@ -125,7 +138,6 @@ class ZaloApiService
             $previousUrl = Session::get('previous_url');
 
             return redirect($previousUrl);
-
         } catch (\Exception $e) {
             Log::error("get access token: " . $e->getMessage());
             return abort(400);
