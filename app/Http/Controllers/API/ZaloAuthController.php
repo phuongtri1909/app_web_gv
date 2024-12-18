bbcbc23dad2b23234eddff88fba4a9d5eaccb90c
phuongtri1909 <tri2003bt@gmail.com>
2024-12-19 01:11:35 +0700

    update auth zalo

--- app/Http/Controllers/API/ZaloAuthController.php
+++ app/Http/Controllers/API/ZaloAuthController.php
@@ -22,7 +22,29 @@ class ZaloAuthController extends Controller
         $get_info = $request->get('get_info');
         $get_phone = $this->zaloApiService->getPhoneNumber($request->header('access_token'), $request->header('code'));
 
-        return $customer = $this->zaloApiService->updateProfile( $get_info, $get_phone);
+        $customer = Customer::where('id', $get_info['id'])->first();
+
+        if (!$customer) {
+            $customer = new Customer();
+            $customer->id = $get_info['id'];
+            $customer->customerName = $get_info['name'];
+            if (isset($get_phone['data']['number'])) {
+                $customer->phone = $get_phone['data']['number'];
+            }
+            if (isset($get_info['picture']['data']['url'])) {
+                $customer->imageUrl = $get_info['picture']['data']['url'];
+            }
+            $customer->save();
+        }else{
+            $customer->customerName = $get_info['name'];
+            if (isset($get_phone['data']['number'])) {
+                $customer->phone = $get_phone['data']['number'];
+            }
+            if (isset($get_info['picture']['data']['url'])) {
+                $customer->imageUrl = $get_info['picture']['data']['url'];
+            }
+            $customer->save();
+        }
 
         return response()->json([
             'customer' => $customer,
