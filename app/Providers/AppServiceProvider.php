ced1c302372fe2c8024e950e28cf0b686ce9d185
phuongtri1909 <tri2003bt@gmail.com>
2024-12-24 01:09:53 +0700

    task count menu pending

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,13 +3,25 @@
 namespace App\Providers;
 
 use App\Models\Tab;
+use App\Models\Business;
 use App\Models\Language;
+use App\Models\Locations;
+use App\Models\LegalAdvice;
+use App\Models\BusinessMember;
+use App\Models\JobApplication;
+use App\Models\ProductBusiness;
 use App\Services\ZaloApiService;
 use Illuminate\Support\Facades\DB;
+use App\Models\BusinessCapitalNeed;
+use App\Models\BusinessRecruitment;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use App\Models\BusinessFairRegistration;
+use App\Models\BusinessStartPromotionInvestment;
+use App\Models\Unit;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -31,9 +43,36 @@ class AppServiceProvider extends ServiceProvider
     
         $languages = Schema::hasTable('languages') ? Language::all() : collect();
 
+        $businessMemberStatusPending = BusinessMember::where('status', 'pending')->count();
+        $businessStatus = Business::where('status', 'pending')->count();
+        $businessProductStatus = ProductBusiness::where('status', 'pending')->count();
+        $capitalNeedsStatus = BusinessCapitalNeed::where('email_status', 'not_sent')->count();
+        $businessRecruitmentStatus = BusinessRecruitment::where('status', 'pending')->count();
+        $jobApplicationsStatus = JobApplication::where('status', 'pending')->count();
+        $legalAdviceStatus = LegalAdvice::where('status', 'pending')->count();
+        $startPromotionInvestmentStatus = BusinessStartPromotionInvestment::where('status', 'pending')->count();
+        $fairRegistrationsStatus = BusinessFairRegistration::where('status', 'pending')->count();
+
+        $unit_id = Unit::where('unit_code','QGV')->first()->id;
+        if($unit_id) {
+            $locationStatus = Locations::where('unit_id', $unit_id)->where('status', 'pending')->count();
+        } else {
+            $locationStatus = 0;
+        }
+
         
         $sharedData = [
-            'languages' => $languages,   
+            'languages' => $languages,
+            'businessMemberStatusPending' => $businessMemberStatusPending,
+            'businessStatus' => $businessStatus,
+            'businessProductStatus' => $businessProductStatus,
+            'capitalNeedsStatus' => $capitalNeedsStatus,
+            'businessRecruitmentStatus' => $businessRecruitmentStatus,
+            'jobApplicationsStatus' => $jobApplicationsStatus,
+            'legalAdviceStatus' => $legalAdviceStatus,
+            'startPromotionInvestmentStatus' => $startPromotionInvestmentStatus,
+            'fairRegistrationsStatus' => $fairRegistrationsStatus,
+            'locationStatus' => $locationStatus,
         ];
 
         View::composer(['layouts.app', 'admin.layouts.app'], function ($view) use ($sharedData) {
